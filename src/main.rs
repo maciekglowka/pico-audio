ec6cf0e59756ff89f03c6eabfe78134955658cc6
maciek.glowka <maciek.glowka@gmail.com>
2026-03-10 22:21:32 +0100

    usb serial

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use embedded_hal::{delay::DelayNs, digital::OutputPin};
 use panic_halt as _;
 use rp235x_hal as hal;
 
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
+
 #[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
@@ -38,12 +41,42 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("maciek")
+            .product("pico-audio")
+            .serial_number("001")])
+        .unwrap()
+        .max_packet_size_0(64)
+        .unwrap()
+        .device_class(2)
+        .build();
+
     let mut led_pin = pins.gpio25.into_push_pull_output();
+
     loop {
-        led_pin.set_high().unwrap();
-        timer.delay_ms(500);
-        led_pin.set_low().unwrap();
-        timer.delay_ms(500);
+        // led_pin.set_high().unwrap();
+        let _ = serial.write(b"UP\r\n");
+        // timer.delay_ms(500);
+        // led_pin.set_low().unwrap();
+        // let _ = serial.write(b"DOWN");
+        // timer.delay_ms(500);
+
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                _ => (),
+            }
+        }
     }
 }
 
